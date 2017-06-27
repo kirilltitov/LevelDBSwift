a958e900e0201028ade32b6f9152f5678fb5cadb
Kirill Titov <work@kirilltitov.com>
2017-06-27 21:54:28 +0300

    Changed subpackage

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "LevelDB",
     dependencies: [
-        .Package(url: "https://github.com/jjacobson93/CLevelDB", majorVersion: 1)
+        .Package(url: "https://github.com/kirilltitov/CLevelDB", majorVersion: 1)
     ]
 )
